6f7da8c7577daab569ad33f7e25c11a55bb7daec
GWLOCAL\32174918 <beewt@naver.com>
2021-12-31 15:21:37 +0900

    title changed

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: `zoomkoding.com`,
-  description: `줌코딩의 개발일기`,
+  title: `kyeongju.com`,
+  description: `경주의 개발일기`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
   siteUrl: `https://www.zoomkoding.com`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
@@ -11,16 +11,16 @@ module.exports = {
   },
   ga: 'UA-134826755-2', // Google Analytics Tracking ID
   author: {
-    name: `정진혁`,
+    name: `허경주`,
     bio: {
       role: `개발자`,
       description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
       thumbnail: 'zoomkoding.gif', // Path to the image in the 'asset' folder
     },
     social: {
-      github: `https://github.com/zoomkoding`,
+      github: `https://github.com/heokyeongju`,
       linkedIn: `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
-      email: `zoomkoding@gmail.com`,
+      email: `beewt@naver.com`,
     },
   },
 
@@ -43,8 +43,8 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        date: '2018.04 ~',
-        activity: '개발 시작 & 블로그 시작',
+        date: '2021.12.31 ~',
+        activity: '블로그 시작',
       },
       {
         date: '2018.07 ~ 2018.11',
